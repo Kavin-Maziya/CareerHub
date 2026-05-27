1a2b4e9f2f7de2762767fed0c5f5742a19bd71b2
Kavin Maziya <kavinmaziya256@gmail.com>
2026-05-27 03:54:05 +0200

    updated the HTTP get methods with suitable Return Statuses

---
+++ APIs/Controllers/JobsController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using APIs.Models;
+using APIs.Data;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JobsController(JobListingStore jobService) : ControllerBase
+{
+
+    // Returns all available job listings
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<JobListing>>> GetJobsAsync()
+    {
+        // Calls the service to Get all jobs
+        var jobs = await jobService.GetAllJobsAsync();
+
+        // Return HTTP 200 OK status with the job data
+        return Ok(jobs);
+    }
+
+
+    // Returns a single job listing by ID
+    [HttpGet("{id}")]
+    public async Task<ActionResult<JobListing>> GetJobByIdAsync(int id)
+    {
+        // Call the service to Get a job by ID
+        var job = await jobService.GetJobByIdAsync(id);
+
+        // Return HTTP 404 status if the job does not exist
+        if (job is null)
+        {
+            return NotFound();
+        }
+
+        // Return HTTP 200 OK status with the job data
+        return Ok(job);
+    }
+}
