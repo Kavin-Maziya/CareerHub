abd36e19761a54ec8d4dbe8f7e75d3c2cd4db542
Kavin Maziya <kavinmaziya256@gmail.com>
2026-05-27 03:57:02 +0200

    updated and commented out unnecessary code

--- APIs/Program.cs
+++ APIs/Program.cs
@@ -1,17 +1,17 @@
 using Microsoft.AspNetCore.Mvc.Diagnostics;
 using Scalar.AspNetCore; 
-using  APIs.Services; // Imports JobService class with dummy data from the Services folder
- 
- //Phase 1 : Builder - Register the services into the app
- /// Dependency injection container
- 
- var builder = WebApplication.CreateBuilder(args);
+using APIs.Data; // Imports JobListingStore class with dummy data
+
+//Phase 1 : Builder - Register the services into the app
+/// Dependency injection container
+
+var builder = WebApplication.CreateBuilder(args);
 
  //Register Your services
 
  builder.Services.AddControllers(); //registering controller support
  builder.Services.AddOpenApi(); // Registering built-in OpenApi document generation
- builder.Services.AddSingleton<JobServices>(); // Registers JobServices as a Singleton service that will be used as a single instance the entire project
+ builder.Services.AddSingleton<JobListingStore>(); // Registers JobServices as a Singleton service that will be used as a single instance the entire project
 
 
  var app = builder.Build(); //Nothing can be regsitered after this
@@ -27,25 +27,25 @@ if (app.Environment.IsDevelopment())
 app.MapControllers(); 
 
 // Returns all available jobs
-app.MapGet("/jobs", async (JobServices jobService) =>
-{
-    var jobs = await jobService.GetAllJobsAsync();
+// app.MapGet("/jobs", async (JobListingStore jobService) =>
+// {
+//     var jobs = await jobService.GetAllJobsAsync();
 
-    return Results.Ok(jobs);
-});
+//     return Results.Ok(jobs);
+// });
 
- // Returns a single job listing by ID
-app.MapGet("/jobs/{id}", async (int id, JobServices jobService) =>
-{
-    var job = await jobService.GetJobByIdAsync(id);
+//  // Returns a single job listing by ID
+// app.MapGet("/jobs/{id}", async (int id, JobListingStore jobService) =>
+// {
+//     var job = await jobService.GetJobByIdAsync(id);
 
-    if (job is null)
-    {
-        return Results.NotFound(); // Return HTTP 404 status if job does not exist
-    }
+//     if (job is null)
+//     {
+//         return Results.NotFound(); // Return HTTP 404 status if job does not exist
+//     }
 
-    return Results.Ok(job); // Return HTTP 200 OK status with the data
-});
+//     return Results.Ok(job); // Return HTTP 200 OK status with the data
+// });
 
 
 
