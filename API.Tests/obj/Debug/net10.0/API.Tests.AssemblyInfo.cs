fe4585f3ab2efba712a23b49217729bdb3065237
Kavin Maziya <kavinmaziya256@gmail.com>
2026-06-23 09:57:53 +0200

    added JobListing form and updated the Home Layout

--- API.Tests/obj/Debug/net10.0/API.Tests.AssemblyInfo.cs
+++ API.Tests/obj/Debug/net10.0/API.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95c1fbeeed87c96ac3868f777a9774a50d642571")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d11563a1796f426972cd0026529d90201fd887da")]
 [assembly: System.Reflection.AssemblyProductAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
